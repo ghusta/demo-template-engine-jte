0dcae9557d9f62d34c1955d182dfc383f0583bd8
Guillaume HUSTA <guillaume.husta@gmail.com>
2025-05-15 14:43:14 +0200

    feat(spring-mvc): Update GlobalModelAttributes to inject locale into Model

--- src/main/java/org/example/controller/GlobalModelAttributes.java
+++ src/main/java/org/example/controller/GlobalModelAttributes.java
@@ -3,6 +3,9 @@ package org.example.controller;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.servlet.support.RequestContextUtils;
+
+import java.util.Locale;
 
 @ControllerAdvice
 public class GlobalModelAttributes {
@@ -13,6 +16,16 @@ public class GlobalModelAttributes {
     @ModelAttribute("contextPath")
     public String contextPath(HttpServletRequest request) {
         return request.getContextPath();
+    }
+
+    /**
+     * Injects object 'locale' into {@link org.springframework.ui.Model}.
+     *
+     * @see RequestContextUtils#getLocale(HttpServletRequest)
+     */
+    @ModelAttribute("locale")
+    public Locale currentLocale(HttpServletRequest request) {
+        return RequestContextUtils.getLocale(request);
     }
 
 }
